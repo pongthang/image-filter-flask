48e9259d6913c0347a4b63d3ff8827d8b24c12a4
abhishekparmar <abhishekparmaroriginal@gmail.com>
2025-03-22 14:35:23 +0530

    fix:fixed cores error

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from flask_cors import CORS
 from apis.product_apis import product_api
 from routes.main import main_bp  # Importing blueprint
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
+CORS(app)
 
 # Register Blueprints
 app.register_blueprint(main_bp)
